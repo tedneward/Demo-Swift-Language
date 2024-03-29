fca54381e7dd92ed481d1cbe41cb194081fde861
Ted Neward <ted@tedneward.com>
2024-03-29 14:29:12 -0700

    Added some string examples

--- basics.swift
+++ basics.swift
@@ -9,8 +9,24 @@ var moneyD = 42000
 moneyD = 50000
 // {{## END locals ##}}
 
+// {{## BEGIN string-initialization ##}}
+let singleLineString = "These are the same."
+let multilineString = """
+These are the same.
+"""
+let softWrappedQuotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin, \
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on \
+till you come to the end; then stop."
+"""
+let threeMoreDoubleQuotationMarks = #"""
+Here are three more double quotes: """
+"""#
+// {{## END string-initialization ##}}
+
 // {{## BEGIN string-interpolation ##}}
 let age = 42
 let message = "He is \(age) years old"
-print(message)
 // {{## END string-interpolation ##}}
